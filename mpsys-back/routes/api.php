c868951c0eb240e00e1871981fb79cb6b9ffc69b
isaacfariaas <farias.isaac321@gmail.com>
2022-07-03 18:45:10 -0300

    refactor: use laravel sanctum to login auth

---
+++ mpsys-back/routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    $user = Auth::user();
+    return response()->json($user,200);
+});
+
+Route::post('/login', function(Request $request){
+
+    if (Auth::attempt(['email'=> $request->email,'password'=> $request->password])) {
+
+        $user = Auth::user();
+        $token = $user->createToken('JWT');
+
+        return response()->json($token->plainTextToken,200);
+    }
+    return response()->json('Usuario invalido', 401);
+});
