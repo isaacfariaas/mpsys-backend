f336bfc2fed44ab7ec994c947a4e81821d6d1ad2
isaacfariaas <farias.isaac321@gmail.com>
2022-08-07 20:44:41 -0300

    add coment

--- mpsys-back/database/seeders/DatabaseSeeder.php
+++ mpsys-back/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-
+        // todas as senhas são "password"
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
